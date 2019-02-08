b77ee9d999d4d28e833376baca95f4be130a669f
문한주(163229) <luvet@shinsegae.com>
2019-02-08 21:41:31 +0900

    감시문으로 리팩토링

--- javascript/caller.js
+++ javascript/caller.js
@@ -14,25 +14,27 @@ var SimpleCaller = {
 // 레거시 JS 기준으로 작성 되었습니다. 나중에 ES6으로 리팩토링 해봐요 ^_^
 var Caller = (function() {
     var execute = function(obj) {
-        if(typeof obj !== "object" || null === obj) { // 파라미터 체크 (방어로직)
-            console.warn(">> Caller.execute의 파라미터는 배열 혹은 객체만 가능합니다.");
+        if(typeof obj !== "object" || null === obj) { // 객체여부 체크 (방어로직)
+            console.warn(">> Caller.execute의 파라미터는 객체(배열포함)만 허용합니다.");
             return;
         }
 
         for(var key in obj) {
-			if(obj.hasOwnProperty(key)) { // 상속된 속성은 패스! (방어로직)
-                if(typeof obj[key] === 'function') {
-                    console.group("========== " + key + " ==========");
-                    try {
-                        obj[key]();
-                    } catch(e) { // 오류가 나면, 해당 메소드만 에러 출력하고 계속 실행
-                        console.error(key + " > " + e.name + " : " + e.message);
-                    }
-                    console.groupEnd();
-                } else {
-                    console.warn(">> 속성 '" + key + " : " + obj[key] + "'는 함수가 아닙니다.");
-                }
+            if(!obj.hasOwnProperty(key)) { // 상속된 속성/메소드 패스 (방어로직)
+                continue;
             }
+            if(typeof obj[key] !== 'function') { // 값의 함수여부 체크 (방어로직)
+                console.warn(">> 속성 '" + key + " : " + obj[key] + "'는 함수가 아닙니다.");
+                continue;
+            }
+
+            console.group("========== " + key + " ==========");
+            try {
+                obj[key]();
+            } catch(e) { // 오류가 나면, 해당 메소드만 에러 출력하고 다음 계속 실행
+                console.error(key + " > " + e.name + " : " + e.message);
+            }
+            console.groupEnd();
         }
     }
 

3149cb90cb029a16646f5b02b4c564a074d6d17b
문한주(163229) <luvet@shinsegae.com>
2019-02-13 13:28:53 +0900

    코드 정리 및 주석 보강

--- javascript_back/simpleServer.js
+++ javascript_back/simpleServer.js
@@ -1,20 +1,23 @@
-// HTML 컨텐츠 직접 문자열에 하드코딩 (샘플)
-var html = `
-<p>Hello, JS!</p>
-<p>Welcome ^_^</p>
+// 직접 문자열로 HTML 컨텐츠 하드코딩
+var html = // 백틱(`~~`)은 둘러싼 모든 텍스트를 문자열로 취급합니다~
+`
+<div id="app">
+    <p>Hello, Node.js!</p>
+    <p>Welcome ^_^</p>
+</div>
 `;
 
-// var html = 
+// var html =
 // require("fs") // Node.js 내장모듈 중 파일시스템 모듈 임포트
 // .readFileSync("../index.html", { encoding: "UTF-8" }); // 파일읽기(동기)
-// 원래는 비동기(.readFile)이나, 동기메소드를 별도로 제공하는 거예요!
+// 원래는 비동기(.readFile)인데, 동기메소드를 별도로 제공하는 거예요!
 
 require("http") // Node.js 내장모듈 중 웹서버 모듈 임포트
-.createServer((req, res) => {
+.createServer((req, res) => { // 서버 생성
 	res.statusCode = 200;
 	res.setHeader("Content-Type", "text/html");
 	res.end(html);
 })
-.listen(80, "localhost", () => {
+.listen(80, "localhost", () => { // 서버 시작
 	console.log(">> SimpleServer is running!");
 });

c7c8cfc34b176354076941139f859c87e7b0e148
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2019-09-04 06:21:31 +0900

    react project created

---
+++ hello-react/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+import Counter from './Counter';
+import './App.css';
+
+class App extends Component {
+  render() {
+   
+    return (
+      <Counter/>
+    );
+  }
+}
+
+export default App;
---
+++ hello-react/src/MyName.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const MyName  = ({ name }) => {
+  return (
+    <div>안녕하세요 제 이름은  {name} 입니다.</div>
+  );
+}
+
+MyName.defaultProps = {
+  name : '기본'
+}
+
+export default MyName;

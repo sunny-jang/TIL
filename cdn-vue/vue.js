eec598f96492e68c837de62b5961a52ccf6c9393
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2019-09-08 06:49:27 +0900

    added Vue directive

--- cdn-vue/vue.js
+++ cdn-vue/vue.js
@@ -3,7 +3,8 @@ var app = new Vue({
   el: '#app', // select Element
   //data has the information which we are going to use on view.
   data: {
-    name: 'Vue'
+    name: '<i>이탈릭</i>',
+    value: 0,
   }
 
 

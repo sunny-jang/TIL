dba42f71269a41882936729bca9dd41633222d93
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2019-09-08 07:27:30 +0900

    V-bind & V-for

--- cdn-vue/vue.js
+++ cdn-vue/vue.js
@@ -3,9 +3,16 @@ var app = new Vue({
   el: '#app', // select Element
   //data has the information which we are going to use on view.
   data: {
-    name: '<i>이탈릭</i>',
+    name: 'Vue',
     value: 0,
+    smile: true,
+    feelsgood: 'https://sites.psu.edu/siowfa16/files/2016/10/YeDYzSR-10apkm4.png',
+    feelsbad: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ2nJ20jBf_gzEzpGF1zo5zk-w4GyQihYKrobpJHieuGQb7qZnoQ',
+    todos: [
+      {text: 'Writing the Vue.js tutorial' },
+      {text: 'Learning about Webpack2' },
+      {text: 'Start a side project' },
+
+    ]
   }
-
-
 })

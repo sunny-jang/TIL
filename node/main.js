3d56e2472fb836bed2bfe0cd2114018c79eb4ead
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2019-09-12 09:11:10 +0900

    Created new server by node

---
+++ node/main.js
@@ -0,0 +1,20 @@
+/* Hello World in NodeJS */
+console.log("Hello, World!");
+var http = require("http");
+
+http.createServer(function(request, response) {
+  /*
+    HTTP 헤더 전송
+    HTTP Satus: 200 : OK
+    Content Type: text/plain
+  */
+ response.writeHead(200, {'Content-type' : 'text/plain'});
+
+ /*
+    Response Body 를 "Hello World"로 설정
+ */
+
+ response.end("Hello World\n")
+}).listen(8081);
+
+console.log("Sever running at http://127.0.0.1:8081");

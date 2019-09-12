5d93ee6114fa9a5dde70d69141ca0cee0e8ee350
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2019-09-12 10:04:25 +0900

    Node blocking & none-blocking code

--- node/main.js
+++ node/main.js
@@ -1,5 +1,5 @@
 /* Hello World in NodeJS */
-console.log("Hello, World!");
+// console.log("Hello, World!");
 var http = require("http");
 
 http.createServer(function(request, response) {
@@ -14,7 +14,27 @@ http.createServer(function(request, response) {
     Response Body 를 "Hello World"로 설정
  */
 
- response.end("Hello World\n")
+ response.end("")
 }).listen(8081);
+//console.log("Sever running at http://127.0.0.1:8081");
 
-console.log("Sever running at http://127.0.0.1:8081");
+var fs = require("fs");
+/*
+
+  Blocking code 
+
+  var data = fs.readFileSync('input.txt');
+
+  console.log(data.toString());
+  console.log("Program has ended");
+
+*/
+
+fs.readFile('input.txt', function(err, data) {
+  if(err) return console.error(err);
+  console.log(data.toString());
+});
+
+console.log("Program has ended")
+
+
